a0ae2e6cd103fc15144373b932385e1cd7056c38
Junho0111 <kt27473067@gmail.com>
2026-02-05 20:46:34 +0900

    feat: 회원 엔티티 및 임시 저장소 구현

---
+++ src/main/java/com/board/domain/member/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.board.domain.member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    public Member save(Member member);
+
+    public Member findById(Long id);
+
+    public Optional<Member> findByLoginId(String loginId);
+
+    public List<Member> findAll();
+}
