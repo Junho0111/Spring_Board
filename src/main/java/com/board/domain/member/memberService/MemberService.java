1e8914d9614baa913c1916f5389e9feebbd1e740
Junho0111 <kt27473067@gmail.com>
2026-02-06 00:10:42 +0900

    feat: 회원 서비스 비즈니스 로직 구현 및 주석 추가

---
+++ src/main/java/com/board/domain/member/memberService/MemberService.java
@@ -0,0 +1,51 @@
+package com.board.domain.member.memberService;
+
+import com.board.domain.member.Member;
+import com.board.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 회원 관련 비즈니스 로직을 처리하는 서비스 클래스입니다.
+ */
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 새로운 회원을 가입시킵니다.
+     * 동일한 로그인 ID를 가진 회원이 이미 존재하면 {@link IllegalStateException}을 발생시킵니다.
+     * @param member 가입할 회원 정보
+     * @return 가입된 회원 객체
+     * @throws IllegalStateException 이미 존재하는 아이디인 경우
+     */
+    public Member join(Member member) {
+        memberRepository.findByLoginId(member.getLoginId())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 아이디입니다.");
+                });
+
+        return memberRepository.save(member);
+    }
+
+    /**
+     * 모든 회원 목록을 조회합니다.
+     * @return 모든 회원 정보를 담은 리스트
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 주어진 ID로 회원을 조회합니다.
+     * @param id 조회할 회원의 ID
+     * @return 조회된 회원 객체
+     */
+    public Member findMemberById (Long id) {
+        return memberRepository.findById(id);
+    }
+}
