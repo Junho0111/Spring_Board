a0e803aefb31aea2c483b5ce06a93bb8935c7396
Junho0111 <kt27473067@gmail.com>
2026-02-06 01:44:23 +0900

    feat: 회원 가입 컨트롤러 생성 및 등록 폼 매핑 추가

---
+++ src/main/java/com/board/web/member/MemberController.java
@@ -0,0 +1,24 @@
+package com.board.web.member;
+
+import com.board.domain.member.Member;
+import com.board.domain.member.memberService.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "members/addMemberForm";
+    }
+}
