e58c7d190c0f381590d86e6b7a79d3892eb152d7
Junho0111 <kt27473067@gmail.com>
2026-02-06 19:28:03 +0900

    test: 로그인 서비스 성공/실패 검증 테스트

---
+++ src/test/java/com/board/domain/login/LoginServiceTest.java
@@ -0,0 +1,57 @@
+package com.board.domain.login;
+
+import com.board.domain.member.Member;
+import com.board.domain.member.MemberRepositoryImpl;
+import com.board.domain.member.memberService.MemberService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LoginServiceTest {
+
+    MemberService memberService;
+    MemberRepositoryImpl memberRepository;
+    LoginService loginService;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemberRepositoryImpl();
+        memberService = new MemberService(memberRepository);
+        loginService = new LoginService(memberRepository);
+    }
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+    @Test
+    void 로그인_성공() {
+        //given
+        Member member = new Member("testID", "test", "testPW");
+        memberService.join(member);
+
+        //when
+        Member loginResult = loginService.login("testID", "testPW");
+
+        //then
+        assertThat(loginResult).isNotNull();
+        assertThat(loginResult.getLoginId()).isEqualTo(member.getLoginId());
+        assertThat(loginResult.getPassword()).isEqualTo(member.getPassword());
+
+    }
+
+    @Test
+    void 로그인_실패() {
+        //given
+        Member member = new Member("testID", "test", "testPW");
+        memberService.join(member);
+
+        //when
+        Member loginResult = loginService.login("로그인실패", "로그인실패");
+
+        //then
+        assertThat(loginResult).isNull();
+    }
+}
