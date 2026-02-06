ba466a10db164ac44b7f09e54b5916f9808982bc
Junho0111 <kt27473067@gmail.com>
2026-02-07 03:55:58 +0900

    feat: 게시물 엔티티 구현 및 로그인 홈 폼 수정

---
+++ src/main/java/com/board/domain/post/Post.java
@@ -0,0 +1,30 @@
+package com.board.domain.post;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**게시물 정보를 나타내는 도메인 객체*/
+@Data
+@NoArgsConstructor
+public class Post {
+
+    /** 게시물 고유 ID */
+    private Long id;
+
+    /** 게시물 제목 */
+    private String title;
+
+    /** 게시물 내용 */
+    private String content;
+
+    /** 게시물 작성자 */
+    private String author;
+
+
+    public Post(Long id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
