9687911910ac9966e1381860d497b33b6b5fed7b
Junho0111 <kt27473067@gmail.com>
2026-02-07 22:32:51 +0900

    fest: 게시물 임시 저장소 기능 검증 테스트

---
+++ src/test/java/com/board/domain/post/PostRepositoryImplTest.java
@@ -0,0 +1,121 @@
+package com.board.domain.post;
+
+import com.board.domain.member.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class PostRepositoryImplTest {
+
+    PostRepositoryImpl postRepository = new PostRepositoryImpl();
+
+    @AfterEach
+    void afterEach() {
+        postRepository.clearStore();
+    }
+
+    @Test
+    void 게시글_등록() {
+        //given
+        Post post = new Post("test", "test", "test");
+
+        //when
+        postRepository.save(post);
+
+        //then
+        assertThat(postRepository.findById(post.getId())).isEqualTo(post);
+    }
+
+    @Test
+    void 게시글_수정_성공() {
+        //given
+        Post oldPost = new Post("기존 제목", "기존 내용", "작성자");
+        Post updatePost =  new Post("수정될 제목", "수정될 내용", "작성자");
+        postRepository.save(oldPost);
+
+        //when
+        postRepository.update(oldPost.getId(), updatePost.getTitle(), updatePost.getContent());
+
+        //then
+        assertThat(postRepository.findById(oldPost.getId()).getTitle()).isEqualTo(updatePost.getTitle());
+        assertThat(postRepository.findById(oldPost.getId()).getContent()).isEqualTo(updatePost.getContent());
+        assertThat(postRepository.findById(oldPost.getId()).getAuthor()).isEqualTo(updatePost.getAuthor());
+    }
+
+    @Test
+    void 게시글_수정_실패() {
+        // given
+        Long notSaveId = 999L;
+        String updateTitle = "수정용 제목";
+        String updateContent = "수정용 내용";
+
+        // when & then
+        assertThatThrownBy(() -> {
+            postRepository.update(notSaveId, updateTitle, updateContent);
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("수정 실패: 해당 ID(" + notSaveId + ")의 게시물이 존재하지 않습니다.");
+    }
+
+    @Test
+    void 게시글_삭제_성공() {
+        // given
+        Post post = new Post("삭제될 제목", "삭제될 내용", "작성자");
+        postRepository.save(post);
+
+        // when
+        Post deletedPost = postRepository.delete(post.getId());
+
+        // then
+        assertThat(deletedPost.getId()).isEqualTo(post.getId());
+        assertThat(postRepository.findById(post.getId())).isNull();
+        assertThat(postRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    void 게시글_삭제_실패() {
+        // given
+        Long notSaveId = 999L;
+
+        // when & then
+        assertThatThrownBy(() -> {
+            postRepository.delete(notSaveId);
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("삭제 실패: 해당 ID(" + notSaveId + ")의 게시물이 존재하지 않습니다.");
+    }
+
+    @Test
+    void 모든_게시글_반환() {
+        //given
+        Post post = new Post("test", "test", "test");
+        Post post2 = new Post("test1", "test1", "test1");
+
+        postRepository.save(post);
+        postRepository.save(post2);
+
+        //when
+        List<Post> posts = postRepository.findAll();
+
+        //then
+        assertThat(posts.size()).isEqualTo(2);
+        assertThat(posts).contains(post, post2);
+    }
+
+    @Test
+    void 게시물_검색_ID() {
+        //given
+        Post post = new Post("test", "test", "test");
+
+        //when
+        Post savePost = postRepository.save(post);
+
+        //then
+        Post findMember = postRepository.findById(post.getId());
+        assertThat(findMember).isEqualTo(savePost);
+    }
+}
