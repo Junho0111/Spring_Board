26ed7e951e7ff7c73a8c68a667d84489bf521fdf
Junho0111 <kt27473067@gmail.com>
2026-02-09 05:39:28 +0900

    feat: 로그 인터셉터 수정

--- src/main/java/com/board/web/interceptor/LogInterceptor.java
+++ src/main/java/com/board/web/interceptor/LogInterceptor.java
@@ -10,21 +10,27 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.UUID;
 
 /**
- * 요청의 시작부터 끝까지 로그를 남기는 인터셉터.
- * 각 요청에 고유한 ID를 부여하여 요청의 흐름을 추적합니다.
+ * 모든 HTTP 요청에 대해 로그를 기록하는 인터셉터.
+ * 요청 시작 시 고유 ID를 부여하여 요청의 전 생애주기를 추적할 수 있도록 지원합니다.
  */
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
-    // 요청 고유 ID를 저장하는 데 사용될 속성 이름
-    public static final String LOGIN_ID = "loginId";
+    // 요청 고유 ID를 HTTP 요청 속성에 저장할 때 사용되는 키
+    public static final String LOGIN_ID = "logId";
 
     /**
      * 컨트롤러 실행 전 호출됩니다.
      * 요청에 고유한 ID를 부여하고, 요청 정보를 로깅합니다.
+     *
+     * @param request 현재 HTTP 요청 객체
+     * @param response 현재 HTTP 응답 객체
+     * @param handler 실행될 핸들러 (컨트롤러 메서드 등)
+     * @return 다음 인터셉터 또는 컨트롤러로 요청을 계속 진행할지 여부 (true: 진행, false: 중단)
+     * @throws Exception 예외 발생 시
      */
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
 
         String uuid = UUID.randomUUID().toString();
@@ -46,7 +52,8 @@ public class LogInterceptor implements HandlerInterceptor {
      */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        log.info("postHandle [{}]", modelAndView);
+        String logId = (String) request.getAttribute(LOGIN_ID);
+        log.info("POST HANDLE [{}][{}][{}]", logId, handler, modelAndView);
     }
 
     /**
@@ -58,9 +65,9 @@ public class LogInterceptor implements HandlerInterceptor {
         String requestURI = request.getRequestURI();
         String logId = (String) request.getAttribute(LOGIN_ID);
 
-        log.info("RESPONSE [{}][{}]", logId, requestURI);
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
         if (ex != null) {
-            log.error("afterCompletion error [{}][{}]", logId, ex.getMessage());
+            log.error("AFTER COMPLETION error [{}][{}][{}]", logId, requestURI, ex);
         }
     }
 
