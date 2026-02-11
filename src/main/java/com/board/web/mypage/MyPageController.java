908b57f501517b293d94601cd4a7b6cbe1cee4d2
Junho0111 <kt27473067@gmail.com>
2026-02-12 03:51:41 +0900

    feat: 마이페이지 컨트롤러 메서드 추가
    - 내가 쓴 글을 받아와서 정보를 model에 담아 넘겨주는 myPostsList 추가
    
    - 이후 구현할 myPostsList.html에서 이 메서드로부터 받은 model을 이용하여 화면에 뿌림

--- src/main/java/com/board/web/mypage/MyPageController.java
+++ src/main/java/com/board/web/mypage/MyPageController.java
@@ -1,6 +1,7 @@
 package com.board.web.mypage;
 
 import com.board.domain.member.Member;
+import com.board.domain.post.Post;
 import com.board.domain.post.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
 
 
 @Controller
@@ -24,5 +27,14 @@ public class MyPageController {
         model.addAttribute("loginMember", loginMember);
 
         return "my-page/myPageHome";
+    }
+
+    @GetMapping("/my-posts")
+    public String myPostsList(@SessionAttribute("loginMember") Member loginMember, Model model) {
+        List<Post> posts = postRepository.findByMemberId(loginMember.getId());
+        model.addAttribute("posts", posts);
+        model.addAttribute("loginMember", loginMember);
+
+        return "my-page/myPostsList";
     }
 }
