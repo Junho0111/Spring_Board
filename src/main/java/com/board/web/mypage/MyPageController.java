866758fc17f5b8d06033a9a004efbe883e294951
Junho0111 <kt27473067@gmail.com>
2026-02-12 03:15:38 +0900

    feat: 마이페이지 컨트롤러 구현
    - 마이페이지 홈화면을 호출하는 기능 추가
    - 사용자 정보를 model에 담아 홈화면에 사용자 이름 표시
    
    - 마이페이지 홈화면에 사용자 이름 및 정보를 보일 수 있음

---
+++ src/main/java/com/board/web/mypage/MyPageController.java
@@ -0,0 +1,28 @@
+package com.board.web.mypage;
+
+import com.board.domain.member.Member;
+import com.board.domain.post.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/posts/my-page")
+public class MyPageController {
+
+    private final PostRepository postRepository;
+
+    @GetMapping
+    public String myPageHome(@SessionAttribute("loginMember") Member loginMember, Model model) {
+        model.addAttribute("loginMember", loginMember);
+
+        return "my-page/myPageHome";
+    }
+}
