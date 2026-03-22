e11b5f16253d955986e098c483c96c60e9b1f1c1
Junho0111 <kt27473067@gmail.com>
2026-03-22 18:16:20 +0900

    feat: 파일 타입 안정성을 위한 FileTypeEnum 도입 및 도메인 적용 - UploadFile에 FileTypeEnum 반영 - FileTypeEnum을 도입하여 타입 안정성 확보함.

---
+++ src/main/java/com/board/domain/uploadfile/FileTypeEnum.java
@@ -0,0 +1,9 @@
+package com.board.domain.uploadfile;
+
+public enum FileTypeEnum {
+    /** 이미지 파일 */
+    IMAGE,
+
+    /** 첨부 파일 */
+    ATTACHED
+}
--- src/main/java/com/board/domain/uploadfile/UploadFile.java
+++ src/main/java/com/board/domain/uploadfile/UploadFile.java
@@ -1,18 +1,32 @@
 package com.board.domain.uploadfile;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
 public class UploadFile {
 
-    /** 유저가 업로드한 파일명 */
-    private String uploadFileName;
+    /** 업로드파일 고유 ID */
+    private Long id;
+
+    /** 연결된 게시물 ID */
+    private Long postId;
 
     /** 서버 내부에서 관리하는 파일명 */
     private String storeFileName;
 
-    public UploadFile(String uploadFileName, String storeFileName) {
+    /** 유저가 업로드한 파일명 */
+    private String uploadFileName;
+
+    /** 이미지 또는 첨부파일 구분 */
+    private FileTypeEnum fileType;
+
+    public UploadFile(String uploadFileName, String storeFileName, FileTypeEnum fileType) {
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
+        this.fileType = fileType;
     }
 }
